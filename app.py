336760fb7e937fedce1f4527627e017ed97b67c0
Varun Nayak <vnayak1506@gmail.com>
2025-01-31 12:28:29 +0000

    Linking some pages and background for login page

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('loginorregister.html')
+
+
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
