48ebbe4e81af6ecdb3f236a67ad0a98b2573bbb7
Varun Nayak <vnayak1506@gmail.com>
2025-02-13 09:33:24 +0000

    Trialling a few things

--- static/js/slider.js
+++ static/js/slider.js
@@ -3,20 +3,23 @@ const images = document.querySelectorAll('.image-slider img');
 const totalImages = images.length;
 let index = 0;
 
+// Function to update the slider
+function updateSlider() {
+    const translateX = -index * 100;  
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+// Right arrow click event
 document.getElementById('next').addEventListener('click', () => {
     index = (index + 1) % totalImages;
     updateSlider();
 });
 
+// Left arrow click event - FIXED
 document.getElementById('prev').addEventListener('click', () => {
     index = (index - 1 + totalImages) % totalImages;
     updateSlider();
 });
-
-function updateSlider() {
-    const translateX = -index * 100;  // Adjust to match the image width (100% in this case)
-    slider.style.transform = `translateX(${translateX}%)`;
-}
 
 // Auto-slide every 3 seconds
 setInterval(() => {
